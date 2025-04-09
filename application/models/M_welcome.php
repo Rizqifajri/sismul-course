a09a092cefdebaa0e3340696b6c124cf568d5a75
Rizqi Fajri <rizqifajri51@gmail.com>
2025-04-09 09:49:48 +0700

    set up

---
+++ application/models/M_welcome.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class M_welcome extends CI_Model {
+  public function create($id, $filename) {
+    $data = [
+      'id' => $id,
+      'name' => $this->input->post('name', TRUE),
+      'description' => $this->input->post('description', TRUE),
+      'filename' => $filename
+    ];
+    
+    $this->db->insert('post', $data);
+  }
+
+}
